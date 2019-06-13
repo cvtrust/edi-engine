d6d6f7153d6a112650cd2b8d5c1178c89eaafb91
Chad Woskoski <cwoskoski@gmail.com>
2019-06-13 12:22:18 -0700

    Added more requirement for a person.

--- src/ConfigDefinition/Processor/Person.php
+++ src/ConfigDefinition/Processor/Person.php
@@ -6,5 +6,38 @@ namespace CVTrust\EdiEngine\ConfigDefinition\Processor;
 
 interface Person
 {
+    /**
+     * @return bool
+     */
+    public function hasCobra(): bool;
 
+    /**
+     * @return bool
+     */
+    public function hasMedicareA(): bool;
+
+    /**
+     * @return bool
+     */
+    public function hasMedicareB(): bool;
+
+    /**
+     * @return bool
+     */
+    public function hasMedicareAandB(): bool;
+
+    /**
+     * @return bool
+     */
+    public function hadMedicarePreviously(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isInsurancePrimary(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isInsuranceSecondary(): bool;
 }
